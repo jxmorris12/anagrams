ead201c9dd05c86d3610096a54b6a662c7ed04df
Jack Morris <jxmorris12@gmail.com>
2019-08-04 14:35:54 -0400

    prepare package for PyPI upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="english-anagrams",
+    version="0.0.1",
+    author="jxmorris12",
+    author_email="jxmorris12@gmail.com",
+    description="A small package for finding anagrams of an english word",
+    url="https://github.com/jxmorris12/english-anagrams",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
